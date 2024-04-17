697ca32998d4b8bc3ed9402e9a84e0416cbd2d4d
alejandroMorales03 <94929338+alejandroMorales03@users.noreply.github.com>
2024-04-17 03:26:13 -0400

    Complete Sign Up Page. Missing log in link

--- server/server.js
+++ server/server.js
@@ -1,13 +1,19 @@
-import bodyParser from "body-parser";
-import  express from "express"
-
+import express from "express"
+import {dirname} from "path"
+import {fileURLToPath} from "url"
 
 const app = express();
+const __dirname = dirname(fileURLToPath(import.meta.url));
 const port = 3000;
-app.use(bodyParser.urlencoded({extended: true}));
 
-app.listen(3000, () =>{
-    console.log("Server running on port 3000");
+app.use(express.static('public'));
+
+app.get("/", (req, res)=>{
+    
+
+    res.render(__dirname + "/views/signup.ejs")
+
+
 })
 
-
+app.listen(3000);
