26235b4eed0349f94da75c0a23f4f89e1f8520f1
alejandroMorales03 <94929338+alejandroMorales03@users.noreply.github.com>
2024-07-10 18:58:57 -0400

    New folders

---
+++ frontend/parkeasy/Components/LoginForm.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+import loginStyle from '../Styles/loginStyle';
+import {COLORS} from '../Constants/Constants'
+import { useState } from 'react';
+import LinearGradient from 'react-native-linear-gradient';
+
+
+
+
+const LoginForm = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = () => {
+        
+        console.log('Email:', email);
+        console.log('Password:', password);
+      };
+    
+  return (
+    <LinearGradient colors={['#23443B', '#11665F', '#50DACB', '#BCABAE', '#C9C5CB', '#C8D4EC']} style={loginStyle.container}>
+        <View style={loginStyle.form}>
+            <View style={loginStyle.inputConatiner}>
+                <TextInput style={loginStyle.input}
+                           placeholder='Email Address'
+                           keyboardType='email-address'
+                           autoCapitalize='none'
+                           autoCorrect={false}
+                           onChange={setEmail}
+                           value={email}>
+                
+                            
+                </TextInput>
+            </View>
+            <View style={loginStyle.inputConatiner}>
+                <TextInput style={loginStyle.input}
+                           placeholder='Password'
+                           secureTextEntry={true}
+                           onChange={setPassword}
+                           value={password}>
+                    
+                </TextInput>
+            </View>
+
+            <View>
+                <TouchableOpacity style={loginStyle.button}>
+                    <Text>Login</Text>
+                </TouchableOpacity>
+            </View>
+
+
+        </View>
+
+    </LinearGradient>
+    
+  );
+};
+
+export default LoginForm;
---
+++ frontend/parkeasy/Constants/Constants.js
@@ -0,0 +1,12 @@
+export const COLORS =  {
+    BACKGROUND_COLOR: '#005252',
+    FORM_BACKGROUND_COLOR: 'rgba(255, 255, 255, 0.20)',
+    INPUT_FONT_COLOR: '#f3e8ee',
+    BUTTON_COLOR_UNPRESSED: '#9BE9DC'
+    
+}
+
+export const SIZES = {
+    FORM_WIDTH: '80%' 
+}
+ 
