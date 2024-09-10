75be36473f134f73c29494e73ca06f1ef23f82aa
Niel <danielescobar492@gmail.com>
2024-09-10 19:19:22 -0400

    Added Structure Default to Main Page

--- frontend/parkeasy/Components/MainPage.js
+++ frontend/parkeasy/Components/MainPage.js
@@ -16,42 +16,62 @@ import {ICONS} from "../Constants/icons";
 import globalStyles from "../Styles/GlobalStyle";
 import userPagesStyle from "../Styles/UserPagesStyle";
 import GlobalStyle from "../Styles/GlobalStyle";
+import axios from "axios";
+
+//////////////////////////////////// API Configuration ////////////////////////////////////
+
+const API = axios.create({
+    baseURL: 'http://localhost:8000/api/auth',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+});
+
+/////////////////////////////////// COMPONENTS ////////////////////////////////////
+
+
+
+//////////////////////////////////// MAIN COMPONENT ////////////////////////////////////
 
 const mainPage = () => {
     return (
         <SafeAreaView style={Style.mainPageContainer}>
 
-                <View style = {Style.mapContainer}>
-                    <View style = {Style.searchContainer}>
+            <View style = {Style.mapContainer}>
+                <View style = {Style.searchContainer}>
 
-                        <TextInput style = {userPagesStyle.searchInput}
-                                   placeholderTextColor={COLORS.Grey}
-                                   placeholderStyle = {GlobalStyle.input}
-                                   placeholder = "Address"
-                        ></TextInput>
-
-                    </View>
-                    <Text>Map View</Text>
-                </View>
-
-                <View style = {Style.navigationContainer}>
-                    {/*Menu Button*/}
-                    <TouchableOpacity
-                        activeOpacity={0.25}>
-                        <Image source={ICONS.menuLarge} style={Style.menuIcon}></Image>
-                    </TouchableOpacity>
-
-                    {/*Profile Button*/}
-
-                    <TouchableOpacity
-                        activeOpacity={0.25}>
-                        <Image source={ICONS.profileMedium} style={Style.menuIcon}></Image>
-                    </TouchableOpacity>
+                    <TextInput style = {userPagesStyle.searchInput}
+                               placeholderTextColor={COLORS.Grey}
+                               placeholderStyle = {GlobalStyle.input}
+                               placeholder = "Address"
+                    ></TextInput>
 
                 </View>
+                <Text>Map View</Text>
+            </View>
+
+            <View style = {Style.navigationContainer}>
+                {/*Menu Button*/}
+                <TouchableOpacity
+                    activeOpacity={0.25}>
+                    <Image source={ICONS.menuLarge} style={Style.menuIcon}></Image>
+                </TouchableOpacity>
+
+                {/*Profile Button*/}
+
+                <TouchableOpacity
+                    activeOpacity={0.25}>
+                    <Image source={ICONS.profileMedium} style={Style.menuIcon}></Image>
+                </TouchableOpacity>
+
+            </View>
 
         </SafeAreaView>
     )
 }
 
 export default mainPage;
+
+//////////////////////////////////// COMPONENTS ////////////////////////////////////
+
+
